e44dfdb1be10bd9cc4fbb7d2b51e89684c01340d
unknown <mahmoudhassan9933@gmail.com>
2017-06-12 20:26:06 +0200

    add admin folder

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+//require "../index.php";
+?>
+<!-- Add Categories -->
+    <div class="cat">
+      <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        <input type="text" placeholder="Insert Category">
+        <input type="submit" value="Add Category">
+      </form>
+    </div>
+    <!-- End Categories -->
